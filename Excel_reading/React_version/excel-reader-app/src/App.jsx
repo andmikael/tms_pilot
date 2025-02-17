5276b97e537ad7093c68d21638e59a472a48baa9
Heidi Seppi <heidi.seppi@outlook.com>
2025-02-17 10:23:05 +0200

    Created first React version of Excel reader

---
+++ Excel_reading/React_version/excel-reader-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { ExcelReader } from './components/ExcelReader.jsx';
+
+function App() {
+  return (
+    <>
+      <h3>Vite + React: Excel reader example</h3>
+      <ExcelReader/>
+    </>
+  )
+}
+
+export default App
