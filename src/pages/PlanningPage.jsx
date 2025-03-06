6a42b82b88e514205631a6ae95690203546dc255
Juhana Kivelä <32447947+JuhanaKivela@users.noreply.github.com>
2025-03-06 19:37:45 +0200

    Add Leaflet component to root (#2)
    * First working version of dynamic leaflet component
    
    * Finalize first version of leaflet react component on root

--- src/pages/PlanningPage.jsx
+++ src/pages/PlanningPage.jsx
@@ -2,14 +2,18 @@
 * Pääsivu (reittisuunnittelu): Voidaan tarkastella tallennettuja reittejä, lisätä noutopaikkoja niihin
 * ja muodostaa reittisuunnitelmia.
 */ 
+import LeafletMap from "../components/LeafletMap";
 
 const PlanningPage = (data) => {
     
-    return <div>
-        <div className="content">Reittisuunnittelu sivu
-            <p>Järjestelmän reitit: {JSON.stringify(data)}</p>
+    return (
+      <div>
+        <div className="content">
+          Reittisuunnittelu sivu
+          <p>Järjestelmän reitit: {JSON.stringify(data)}</p>
         </div>
-    </div>
+      </div>
+    );
 };
 
 export default PlanningPage;
