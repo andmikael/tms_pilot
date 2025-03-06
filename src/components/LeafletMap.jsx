12db6ff7ccb20ee092fcebaf282f0d1e1e54cf49
Juhana Kivelä <juhana.kivela@tuni.fi>
2025-03-06 20:01:15 +0200

    Delete unnecessary leaflet node files. Move leaflet and leaflet routing machine dirs

--- src/components/LeafletMap.jsx
+++ src/components/LeafletMap.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import MapComponent from './../../route_visualization/src/components/MapComponent';
+import React, { useEffect } from 'react';
 
 const LeafletMap = () => {
+  useEffect(() => {
+    // Check if the map is already initialized
+    if (window.map) {
+      window.map.remove(); // Remove the existing map instance
+      window.map = null;
+    }
+
+    const map = L.map('map').setView([62.7903, 22.8403], 9);
+    window.map = map;
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.Routing.control({
+      waypoints: [
+        L.latLng(62.826941, 22.909949),
+        L.latLng(62.626202, 22.395327),
+        L.latLng(62.424408, 22.173556),
+      ],
+    }).addTo(map);
+  }, []);
+
   return (
     <div>
       <h3>Leaflet Map</h3>
-      <MapComponent />
+      <div id="map" style={{ height: '60vh', width: '80%' }}></div>
     </div>
   );
 };
