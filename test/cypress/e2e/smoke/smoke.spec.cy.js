ac6cbe4fd01342be4b426d34f35ef31b1f6b3a95
Juhana Kivelä <juhana.kivela@tuni.fi>
2025-03-12 10:21:59 +0200

    Debug failing ci test cases

--- test/cypress/e2e/smoke/smoke.spec.cy.js
+++ test/cypress/e2e/smoke/smoke.spec.cy.js
@@ -8,7 +8,7 @@ describe("Smoke tests", () => {
 
   it("should load the main page", () => {
     cy.visit(BASE_URL);
-    cy.contains('Kuljetuksien suunnittelujärjestelmä', { timeout: 10000 }).should('be.visible');
+    cy.contains('Kuljetuksien suunnittelujärjestelmä');
   });
 
   it("should load the 'Tiedostot' page", () => {
