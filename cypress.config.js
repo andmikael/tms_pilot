c08e33cf01a32fcc9e60c9e9f9406a77deaf216e
Juhana Kivelä <juhana.kivela@tuni.fi>
2025-03-12 11:56:17 +0200

    Debuggin ci tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+import path from 'path';
 
 export default defineConfig({
   e2e: {
-    supportFile: "./test/cypress/support/e2e.js",
-    specPattern: "./test/cypress/e2e/**/*.spec.cy.js",
+    supportFile: path.resolve(__dirname, './test/cypress/support/e2e.js'),
+    specPattern: path.resolve(__dirname, './test/cypress/e2e/**/*.spec.cy.js'),
   },
 });
