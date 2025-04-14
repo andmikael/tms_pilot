c6fc01277ed95403acf00a84088db198752b5b07
Juhana Kivelä <juhana.kivela@tuni.fi>
2025-04-14 22:00:19 +0300

    Convert Leaflet to request all routing data at once

--- src/components/LeafletMap.jsx
+++ src/components/LeafletMap.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef } from 'react';
-import PropTypes from 'prop-types'
 import { routePropType } from '../propTypes/routePropType';
 
 const LeafletMap = ({ dataToParent }) => {
@@ -22,48 +21,15 @@ const LeafletMap = ({ dataToParent }) => {
       ...dataToParent
     ];
 
-    const colors = [
-      '#0074D9', // strong blue
-      '#2ECC40', // medium green
-      '#FF851B', // orange
-      '#B10DC9', // purple
-      '#85144b', // maroon
-      '#3D9970', // teal
-      '#555555', // dark gray
-      '#FF4136', // red
-      '#7FDBFF', // light blue
-      '#F012BE', // pink
-      '#01FF70', // neon green
-      '#AAAAAA', // medium gray
-      '#FFDC00', // golden yellow
-      '#39CCCC', // light teal
-      '#111111', // blackish
-      '#F0E68C', // khaki
-      '#8B4513', // saddle brown
-      '#4682B4', // steel blue
-    ];
+    const waypoints = pickUpPointObjects.map(point => L.latLng(point.lat, point.lon));
 
-    pickUpPointObjects.forEach((point, index) => {
-      const nextPoint = pickUpPointObjects[index + 1];
-      if (!nextPoint) return; // skip last point if no pair
-    
-      L.Routing.control({
-        waypoints: [
-          L.latLng(point.lat, point.lon),
-          L.latLng(nextPoint.lat, nextPoint.lon),
-        ],
-        show: false,
-        addWaypoints: false,
-        draggableWaypoints: false,
-        fitSelectedRoutes: false,
-        lineOptions: {
-          styles: [
-            { color: 'black', weight: 4 }, // Base color
-            { color: colors[index % colors.length], weight: 2 }, // "Stroke" around the line
-          ],
-        },
-      }).addTo(map);
-    });
+    L.Routing.control({
+      waypoints: waypoints,
+      show: false,
+      addWaypoints: false,
+      draggableWaypoints: false,
+      fitSelectedRoutes: false,
+    }).addTo(map);
 
 
     pickUpPointObjects.forEach((point, index, array) => {
