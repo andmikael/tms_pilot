431ac1fd603de3e95438d11abc4789940cf76a5e
Juhana Kivelä <juhana.kivela@tuni.fi>
2025-04-21 14:18:31 +0300

    Fix bug TT1

--- src/components/LeafletMap.jsx
+++ src/components/LeafletMap.jsx
@@ -31,27 +31,57 @@ const LeafletMap = ({ dataToParent }) => {
       fitSelectedRoutes: false,
     }).addTo(map);
 
+    // Grouped points are needed as sometimes multiple points are in the same location (coordinates)
+    // Otherwise, the markers would be stacked on top of each other and bottom ones would not be visible
+    const groupedPoints = {};
+
 
     pickUpPointObjects.forEach((point, index, array) => {
+      const key = `${point.lat},${point.lon}`;
+      if (!groupedPoints[key]) {
+        groupedPoints[key] = [];
+      }
+    
       let label;
-
       if (index === 0) {
         label = "Aloituspaikka:";
       } else if (index === array.length - 1) {
-        label = "Lopetuspaikka";
+        label = "Lopetuspaikka:";
       } else {
         label = index.toString() + '.';
       }
-      L.marker([point.lat, point.lon])
+      // Saves the points in a dictionary with the coordinates as the key
+      // The coordinates are in format "lat,lon" (e.g. "62.7903,22.8403")
+      groupedPoints[key].push({
+        label,
+        name: point.name,
+        address: point.address,
+        city: point.city,
+        postalCode: point.postalCode,
+        standardPickup: point.standardPickup,
+      });
+    });
+
+    // Creates markers with (possible) combined labels
+    Object.entries(groupedPoints).forEach(([key, points]) => {
+      const [lat, lon] = key.split(',').map(Number);
+    
+      // Tooltip text (the always visible one) 
+      const tooltipText = points.map(p => `${p.label} ${p.name}`).join('<br>');
+    
+      // Popup text (the one that opens when clicking the marker)
+      const popupText = points.map(p => `
+        ${p.name}<br>
+        ${p.address}<br>
+        ${p.city}, ${p.postalCode}<br>
+        Vakionouto: ${p.standardPickup == 'yes' ? 'Kyllä' : 'Ei'}
+      `).join('<hr>');
+      
+      L.marker([lat, lon])
         .addTo(map)
-        .bindTooltip(`${label} ${point.name}`, { permanent: true, direction: 'right' })
+        .bindTooltip(tooltipText, { permanent: true, direction: 'right' })
         .openTooltip()
-        .bindPopup(
-          `${point.name}<br>
-          ${point.address}<br>
-          ${point.city}, ${point.postalCode}<br>
-          Vakionouto: ${point.standardPickup == 'yes' ? 'Kyllä' : 'Ei'}`
-        );
+        .bindPopup(popupText);
     });
 
     //Removes previous Map container if it already exists
